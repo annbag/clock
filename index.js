fe3b02e4e0fbac5d443a41f99d45a7d4fc47bf9f
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-03 23:24:41 +0200

    add task

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+//ticking clock
+
+function tick() {
+	var app = React.createElement('div', {}, 
+		React.createElement('h1', {}, 'Hello world' ),
+		React.createElement('h1', {}, new Date().toLocaleTimeString() ),
+	);
+	ReactDOM.render(
+		app, 
+		document.getElementById('root')
+	);
+}
+setInterval(tick, 1000);
 
